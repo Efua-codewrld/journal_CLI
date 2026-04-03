b76deb32e4eda9c226e583fed088baaf51263b84
Efua Incoom <incoomefua@gmail.com>
2026-04-03 12:06:05 +0100

    no repitition of asking for filename,also there is an inclusion of the date and time , the mood of the user is also documented

--- journal_CLI.py
+++ journal_CLI.py
@@ -1,16 +1,21 @@
+from datetime import datetime
+#setting a standard variable to hold the entry as the file incase no file has been set
+current_file = None
 while True:
-    choice = input("1. Add entry\n2. View entries\n3. Exit\nchoose: ")
+    choice = input("1. Add entry\n2. View entries\n3. change file\n4. Exit\nchoose: ")
 
     if choice == "1":
+        if current_file is None:
+            current_file = input("Enter filename: ").strip()
+        
+        now = datetime.now()
+        Date = now.strftime("%d/%m/%Y, %H:%M:%S")
+
         journal_entry = input("log in today's journal entry: ")
+        Mood=input("Okay baddie, what's the mood for today: ")
         print("Today's yap session be looking like: ",journal_entry)
-        filename = input("enter a filename that fits your journal vibe: ").strip()
-        if len(filename) < 1:
-            print("filename not entered")
-        else:
-            filename= filename +".txt"
-            with open(filename,"a") as f:
-                f.write(journal_entry + "\n")
+        with open(current_file + ".txt","a") as f:
+            f.write( f" {Date} | MOOD {Mood} | {journal_entry}\n")
 
     if choice == "2":
         filename = input("Enter file name to view: ")
@@ -18,7 +23,11 @@ while True:
             content = f.read()
             print(content)
     
-    if choice == "3":
+    #giving the user an option to set a new file to hold entries
+    elif choice == "3":
+        current_file = input("Enter new filename: ").strip()
+    
+    if choice == "4":
         break
 
     
