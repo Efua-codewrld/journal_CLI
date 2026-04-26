979991654868a6761701f3dcda3eba7c430d625b
Efua Incoom <incoomefua@gmail.com>
2026-04-26 20:12:02 +0100

    Initial version of the mood journal

---
+++ mood_journal_analyzer.py
@@ -0,0 +1,15 @@
+entry_file = input("Enter file name: ")
+if len (entry_file) < 1:
+    print("File name not entered")
+else:
+    filename = entry_file + ".txt"
+    e_file = open(filename,"a")
+    
+    while True:
+        journal_entry = input("Write your journal entry: ")
+
+        if journal_entry.uppercase()=="QUIT":
+            break
+        with open(filename,"a") as e_file:
+            e_file.write (journal_entry + "\n")
+    print(f"All entries saved to {filename}.")
